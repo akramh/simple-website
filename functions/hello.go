4c6088c5b3d8fb79de375e622407ecab1758500a
akramh <akramh@gmail.com>
2021-02-24 18:20:23 -0800

    functions

--- functions/hello.go
+++ functions/hello.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"context"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	lc, ok := lambdacontext.FromContext(ctx)
-	if !ok {
-		return &events.APIGatewayProxyResponse{
-			StatusCode: 503,
-			Body:       "Something went wrong :(",
-		}, nil
-	}
-
-	cc := lc.ClientContext
-
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, " + cc.Client.AppTitle,
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
 	}, nil
 }
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/akramh/simple-website
+
+go 1.16
+
+require github.com/aws/aws-lambda-go v1.22.0 // indirect
